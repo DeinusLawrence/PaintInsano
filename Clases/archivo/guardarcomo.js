4be5756bee0d4b257e677b4eef727411fb7e4a0c
DeinusLawrence <deinuscastro@gmail.com>
2024-03-16 21:02:37 -0700

    Guardar pdf,png y codigo

--- Clases/archivo/guardarcomo.js
+++ Clases/archivo/guardarcomo.js
@@ -7,3 +7,19 @@ function guardarcomo(){
     downloadLink.setAttribute('href',url);
     downloadLink.click();
 }
+
+function guardarcomo1() {
+    // Captura el contenido del canvas como una imagen en formato PNG
+    let imgData = canvaslimpio.toDataURL('image/png');
+
+    // Configura el documento PDF
+    let pdf = new jsPDF();
+    let imgWidth = pdf.internal.pageSize.getWidth();
+    let imgHeight = canvaslimpio.height * imgWidth / canvaslimpio.width;
+
+    // Agrega la imagen al documento PDF
+    pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+
+    // Guarda el PDF
+    pdf.save('CanvasAsPDF.pdf');
+}
